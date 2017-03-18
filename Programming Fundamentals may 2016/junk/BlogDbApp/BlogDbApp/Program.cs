e58821310949661c1c5d0c26edc37f203925f925
alexander-m-ivanov <sasho_matrix@abv.bg>
2017-03-18 11:16:36 +0200

    18.03.2017 sync before QA Automation course
    18.03.2017 sync before QA Automation course

---
+++ Programming Fundamentals may 2016/junk/BlogDbApp/BlogDbApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDbApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new BlogDbContext();
+            var p = new Posts()
+            {
+                Body = "Здравейте, Това ми е първият пост.",
+                Title = "Първи пост",
+                AuthorID = 3,
+                Date = DateTime.Now
+            };
+            db.Posts.Add(p);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Programming Fundamentals may 2016/junk/MVC practice/MVC practice/Controllers/FilesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_practice.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            var path = @"C:\";
+            var all = new List<string>();
+            all.AddRange(Directory.GetFiles(path));
+            all.AddRange(Directory.GetDirectories(path));
+
+            return View(all);
+        }
+    }
+}
